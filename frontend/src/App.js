c7dbb0e9f6290ed01715bb663dc3e29e0244e3b0
Zoe Oliver <zoeoliver@Zoes-MBP.fios-router.home>
2021-10-18 18:57:24 -0400

    lesson 4

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
-
-
+import React from 'react'
+import {Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 function App() {
   return (
     <>
-      <h1>Welcome to Web Design!</h1>
+    <Header />
+    <main>
+      <Container>
+      <HomeScreen/>
+      </Container>
+      </main>
+      <Footer />
     </>
   );
 }
