81418ff8b4208af7c90ee03061b13910558b4840
Zoe Oliver <zoeoliver@Zoes-MacBook-Pro.local>
2021-11-01 14:21:48 -0400

    mern 7

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,18 +2,17 @@ import React from 'react'
 import {Container, Row, Col} from 'react-bootstrap'
 
 const Footer = () => {
-    return (
-        <footer>
+    return <footer>
             <Container>
                 <Row>
                     <Col className = 'text-center py-3'>
-                        Copyright &copy; Web Design
+                        Copyright &copy; ProShop
+
                     </Col>
                 </Row>
-                </Container>
-            Footer
+            </Container>
         </footer>
-    )
+
 }
 
 export default Footer
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,23 +1,35 @@
-import React from 'react'
-import {Navbar, Nav, Container} from 'react-bootstrap'
+import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-    return (
-        <header>
-            <Navbar bg="light" expand="lg">
-  <Container>
-    <Navbar.Brand href="#home">Web Design</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">Link</Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
-        </header>
-    )
-}
+	return (
+		<header>
+			<Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+				<Container>
+					<LinkContainer to="/">
+						<Navbar.Brand>ProShop</Navbar.Brand>
+					</LinkContainer>
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Collapse id="basic-navbar-nav">
+						<Nav className="ms-auto">
+							<LinkContainer to="/cart">
+								<Nav.Link>
+									<i className="fas fa-shopping-cart"></i>Cart
+								</Nav.Link>
+							</LinkContainer>
+							<LinkContainer to="/login">
+								<Nav.Link>
+									<i className="fas fa-user"></i>Sign In
+								</Nav.Link>
+							</LinkContainer>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		</header>
+	);
+};
 
-export default Header
+export default Header;
